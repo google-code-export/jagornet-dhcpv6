e7183d95f002e4de4241e694505e79d3aeb4c59a
agrabil <agrabil@b93dd69a-310d-e06c-c708-a6fd4d7b29e7>
2013-09-07 23:02:51 +0000

    Create 2.0.0-rc tag from trunk

---
+++ dhcpv6/tags/2.0.0-rc/DHCPv6/bin/dhcpv6.bat
@@ -0,0 +1,25 @@
+echo on
+if "%OS%"=="Windows_NT" @setlocal
+
+rem %~dp0 is expanded pathname of the current script under NT
+set DEFAULT_DHCPV6_HOME=%~dp0..
+
+if "%DHCPV6_HOME%"=="" set DHCPV6_HOME=%DEFAULT_DHCPV6_HOME%
+set DEFAULT_DHCPV6_HOME=
+
+:checkJava
+set _JAVACMD=%JAVACMD%
+
+if "%JAVA_HOME%" == "" goto noJavaHome
+if not exist "%JAVA_HOME%\bin\java.exe" goto noJavaHome
+if "%_JAVACMD%" == "" set _JAVACMD=%JAVA_HOME%\bin\java.exe
+goto runApp
+
+:noJavaHome
+if "%_JAVACMD%" == "" set _JAVACMD=java.exe
+echo.
+echo Warning: JAVA_HOME environment variable is not set.
+echo.
+
+:runApp
+"%_JAVACMD%" -cp "%DHCPV6_HOME%\conf";"%DHCPV6_HOME%\lib\*" -Ddhcpv6.home="%DHCPV6_HOME%" com.jagornet.dhcpv6.server.DhcpV6Server %*
